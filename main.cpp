26bfe643606f7aab7af00456a49bce4fbf528a9e
CentOS <centos@centos7.linuxvmimages.local>
2022-06-29 11:44:55 +1000

    Add a new class MyMultiply

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
-#include <iostream>
 #include <Algorithm/MyAdd.hpp>
+#include <Algorithm/MyMultiply.hpp>
+
+#include <iostream>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
     MyNamespace::MyAdd myAdd;
+    std::cout << " 2 + 3 = " << myAdd( 2, 3 ) << std::endl;
 
-    std::cout << myAdd( 2, 3 ) << std::endl;
+    MyNamespace::MyMultiply myMultiply;
+    std::cout << " 2 * 3 = " << myMultiply( 2, 3 ) << std::endl;
 
     return 0;
 }
