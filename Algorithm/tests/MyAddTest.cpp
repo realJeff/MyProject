74a0ca3e5866fc669339e11cfaf5a3f9150e6f38
CentOS <centos@centos7.linuxvmimages.local>
2022-07-07 07:09:29 +1000

    Add INSTANTIATE_TEST_CASE_P

--- Algorithm/tests/MyAddTest.cpp
+++ Algorithm/tests/MyAddTest.cpp
@@ -12,3 +12,27 @@ TEST( MyAddTest, Operator )
     EXPECT_EQ( 5, myAdd( 2, 3 ) );
 }
 
+struct AddParas
+{
+    int value0;
+    int value1;
+    int sum;
+};
+
+struct MyAddFixture : ::testing::TestWithParam< AddParas >
+{};
+
+TEST_P( MyAddFixture, Add )
+{
+    MyNamespace::MyAdd myAdd;
+
+    auto param = GetParam();
+    EXPECT_EQ( param.sum, myAdd( param.value0, param.value1 ) ) << param.value0 << "," << param.value1;
+}
+
+INSTANTIATE_TEST_CASE_P( MyAddTest, MyAddFixture, ::testing::Values(
+        AddParas{ 2, 3, 5 },
+        AddParas{ -1, -3, -4 },
+        AddParas{ -2, 3, 1 },
+        AddParas{ 2, -3, 1 }
+) );
